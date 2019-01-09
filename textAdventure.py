5b584f95e20c0c51d7d2202561d267e5b1d63bb4
68sun <1703208@brunel.ac.uk>
2019-01-09 22:24:51 +0000

    Command list and reworking code

--- textAdventure.py
+++ textAdventure.py
@@ -9,11 +9,11 @@ inventory = {"Placeholder" : 1, "Placeholder2" : 0, "Placeholder3" : 0}
 
 ##Commands
 #A list of commands that the player can use
-commands = ["help", "inventory", "status", "view", "take"]
+commands = {"help" : "Gives a list of commands", "inventory" : "Shows the players inventory", "status" : "Checks the players health and energy level", "inspect" : "inspect a place or item", "desription" : "gives a description of the room", "take": "take an item"}
 
 ##Loop booleans
 #These are the varaibles that decide which loop to run at the moment
-intro = True
+main = True
 
 ##Intro booleans
 #These are the booleans used in the intro loop
@@ -32,19 +32,14 @@ action = "inventory"
 ###These are things that are done or looked at throughout the game by the player
 
 ###Functions
-##Previous action
-#This keeps a record of the previous action to be used so that players don't lose progress while checking their inventory, etc.
-def previousAction():
-    global previous
-    previous = action
-
 
 ##Help
 #Printing a list of general commands that players may be able to use
 def helpCommands():
     print(commands)
     global action
-    action = previous
+    action = "idle"
+    
 
 
 ##Inventory
@@ -53,13 +48,16 @@ def inventoryCheck():
     print("<<<   INVENTORY:   >>>")
     print(inventory)
     global action
-    action = previous
+    action = "idle"
+    
 
 ##Staus
 ##Allows players to check their status/shows status when it changes
 def status():
+    print("<<<<<<<  Status  >>>>>>>>")
     print("<<<   HEALTH: %d/%d   >>>" % (health, maxHealth))
     print("<<<   ENERGY: %d/%d   >>>" % (energy, maxEnergy))
+    print("\n")
 
 
 
@@ -73,12 +71,12 @@ print("You awaken...")
 status()
 
 
-##intro gameplay loop
-while intro:
+##main gameplay loop
+while main:
 
     #This prevents the initial line of the intro from replaying after the first time
     while initialLine:
-        action = input("placeholder text").lower()
+        action = input("Sitting up, you look around the room. Your in a workshop of some kind. At the south wall is a metal large bench, various tools and materials strewn about it. There is a large metal door in the north wall.").lower()
         initialLine = False
 
     
@@ -87,20 +85,24 @@ while intro:
     #This set of code calls the background functions, e.g. to check inventory, find commands, etc.
     if action == ("inventory"):
         inventoryCheck()
+        
 
     elif action == ("help"):
         helpCommands()
 
     elif action == ("status"):
         status()
-        action = previous
+        action = "idle"
+
+    elif action == "idle":
+        action = input("What will you do?").lower()
+        
+        
         
 
-    elif action == ("test"):
-        print("Test")
-        #This saves the previous action entered by the player for use when certain funtions are used
-        previousAction()
-        action = input("What will you do?").lower()
+    elif action == ("description"):
+        print("Your in a workshop of some kind. At the south wall is a metal large bench, various tools and materials strewn about it. There is a large metal door in the north wall.")
+        action = idle
         
 
 
