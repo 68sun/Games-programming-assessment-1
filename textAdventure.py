63cb76378e5474b7c4a9a89fbb8f4095c41cd460
68sun <1703208@brunel.ac.uk>
2019-01-19 22:42:35 +0000

    Inspect command

--- textAdventure.py
+++ textAdventure.py
@@ -1,4 +1,5 @@
 ####Variables
+
 ##Player values
 #These are variables that pertain to the player's "avatar" in the game
 maxHealth = 100
@@ -7,17 +8,21 @@ health = 100
 energy = 100
 inventory = {"Placeholder" : 1, "Placeholder2" : 0, "Placeholder3" : 0}
 
+
 ##Commands
 #A list of commands that the player can use
-commands = {"help" : "Gives a list of commands", "inventory" : "Shows the players inventory", "status" : "Checks the players health and energy level", "inspect" : "inspect a place or item", "desription" : "gives a description of the room", "take": "take an item"}
+commands = {"help" : "Gives a list of general commands the player can use", "inventory" : "Shows the players inventory", "status" : "Checks the players health and energy level", "inspect" : "inspect a place or item", "desription" : "gives a description of the room", "take": "take an item"}
+
 
 ##Loop booleans
 #These are the varaibles that decide which loop to run at the moment
 main = True
 
+
 ##Intro booleans
 #These are the booleans used in the intro loop
 initialLine = True
+
 
 
 ####Setup
@@ -28,8 +33,11 @@ while name == "":
 action = "inventory"
 
 
+
+
 ####Background and functions
 ###These are things that are done or looked at throughout the game by the player
+
 
 ###Functions
 
@@ -42,6 +50,7 @@ def helpCommands():
     
 
 
+
 ##Inventory
 #Allows the player to check their inventory
 def inventoryCheck():
@@ -51,13 +60,57 @@ def inventoryCheck():
     action = "idle"
     
 
+
 ##Staus
-##Allows players to check their status/shows status when it changes
+#Allows players to check their status/shows status when it changes
 def status():
     print("<<<<<<<  Status  >>>>>>>>")
     print("<<<   HEALTH: %d/%d   >>>" % (health, maxHealth))
     print("<<<   ENERGY: %d/%d   >>>" % (energy, maxEnergy))
     print("\n")
+
+
+##Inspect
+#Allows the player to inspect specific parts of the area
+def inspect():
+    global action
+    while action == "inspect":
+        
+        inspection = input("What do you want to inspect?").lower()
+
+        if inspection == "door":
+            print ("Taking a closer look at the metal door, you see it has no handle. Its smooth to the touch, and a thick layer of dust covers it suggesting it hasen't been used in a long while.")
+
+            action = "idle"
+
+        elif inspection == "bench":
+            print ("You look over the bench, shifting through the dust and various junk on it. Among the junk you find a few things of interest.")
+
+            print ("Theres a large spanner, covered in rust and near breaking, but could be used atleast once more.")
+
+            print ("Theres a rectangular object with two prongs sticking out one of the shorter sides and the image of a lightning bolt painted on it. Theres a button on the one of the long side, that causes sparks of electricity to jump out from the prongs.")
+
+            print ("There a large number of steel ball bearings in a massive bag next to the bench.")
+
+            print ("You also find a button on the bottom side of the desk.")
+
+            action = "idle"
+
+        elif inspection == "self":
+            print ("Looking yourself over, you seem to be wearing a survival suit of some kind. Your wearing an entirely yellow suit with no seems and black gloves attached to the forearms. The suit reaches up past your neck and covers your entire head, apart from your face.")
+
+            action = "idle"
+
+        else:
+            print ("This is not a valid subject for inspection")
+    
+
+
+
+
+
+
+
 
 
 
@@ -76,32 +129,47 @@ while main:
 
     #This prevents the initial line of the intro from replaying after the first time
     while initialLine:
-        action = input("Sitting up, you look around the room. Your in a workshop of some kind. At the south wall is a metal large bench, various tools and materials strewn about it. There is a large metal door in the north wall.").lower()
+        print("Sitting up, you look around the room. Your in a workshop of some kind. At the south wall is a metal bench, various tools and materials strewn about it. There is a large metal door in the north wall.")
+
+        print("Type Help for a list of general commands you can use. These are only general commands, the other commands can be found through exploring and experimentation.")
+        
+        action = "idle"
+        
+
         initialLine = False
 
     
     
 
     #This set of code calls the background functions, e.g. to check inventory, find commands, etc.
-    if action == ("inventory"):
+    if action == "inventory":
         inventoryCheck()
         
-
-    elif action == ("help"):
+    elif action == "help":
         helpCommands()
 
-    elif action == ("status"):
+    elif action == "status":
         status()
         action = "idle"
-
+        
+    #This is what every action will default back to
     elif action == "idle":
         action = input("What will you do?").lower()
-        
-        
+
+
+    elif action == "inspect":
+        inspect()
         
 
-    elif action == ("description"):
+
+        
+        
+        
+        
+    #This gives a description of the room, including changes.
+    elif action == "description":
         print("Your in a workshop of some kind. At the south wall is a metal large bench, various tools and materials strewn about it. There is a large metal door in the north wall.")
+
         action = idle
         
 
